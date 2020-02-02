77ee6012d95d651e7e3009c7b2d204a7901213bb
Paul Saikko <paul.saikko@gmail.com>
2020-02-02 17:59:38 +0200

    Clean up plotting

--- train_colorizer.py
+++ train_colorizer.py
@@ -37,7 +37,7 @@ def to_yuv(x): return tf.image.rgb_to_yuv(x / 255.0)
 #
 # Set up tf.data pipeline
 #
-data_batch_size = 1000
+data_batch_size = 12
 STOPSIGN_ID = 11
 n_samples = info.splits["train"].num_examples
 feed = coco_train 
@@ -46,9 +46,10 @@ feed = feed.map(get_image, num_parallel_calls=4)\
            .map(to_yuv, num_parallel_calls=4)\
            .batch(data_batch_size)
 
-
-epochs = 2
-fig, ax = plt.subplots(4,3)
+subplots_w = 4
+subplots_h = 3
+n_subplots = subplots_w * subplots_h
+fig, ax = plt.subplots(subplots_h, subplots_w)
 plt.subplots_adjust(left=0,right=1,bottom=0,top=0.9,wspace=0,hspace=0.2)
 
 example_index = 0
@@ -68,54 +69,28 @@ def show_1ch(x):
     plt.imshow(tf.squeeze(x).numpy(), cmap='Greys', vmin=0, vmax=1)
 
 i = 0
+epochs = 2
 for yuv_images in feed:
     y_data = tf.expand_dims(yuv_images[:,:,:,0], axis=-1)
     uv_data = yuv_images[:,:,:,1:]
 
     model.fit(y_data, uv_data, batch_size=8)
+    uv_pred = model(y_data[:n_subplots])
+    yuv_pred = tf.concat([y_data[:n_subplots],uv_pred], axis=-1)
 
-    for k in range(12):
-        plt.subplot(4,3,k+1)
+    plt.suptitle("Iteration %d" % i)
+
+    for k in range(n_subplots):
+        plt.subplot(subplots_h,subplots_w,k+1)
         plt.cla()
         plt.axis('off')
-
-    fig.suptitle("Iteration %d" % (i+1))
-    plt.subplot(430+2)
-    plt.title("Image")
-    show_yuv(yuv_images[example_index])
-
-    plt.subplot(430+3+1)
-    plt.title("Y")
-    show_1ch(y_data[example_index])
-
-    plt.subplot(430+3+2)
-    plt.title("U")
-    show_2ch(uv_data[example_index,:,:,0])
-
-    plt.subplot(430+3+3)
-    plt.title("V")
-    show_2ch(uv_data[example_index,:,:,1])
-
-    uv_pred = model(tf.expand_dims(y_data[example_index], axis=0))
-
-    plt.subplot(430+3*2+2)
-    plt.title("U'")
-    show_2ch(uv_pred[example_index,:,:,0])
-
-    plt.subplot(430+3*2+3)
-    plt.title("V'")
-    show_2ch(uv_pred[example_index,:,:,1])
-
-    plt.subplot(4,3,11)
-    plt.title("Colorized")
-    yuv_pred = tf.concat([y_data[0],uv_pred[0]], axis=-1)
-    show_yuv(yuv_pred)
+        show_yuv(yuv_pred[k,:,:,:])
 
     plt.pause(0.01)
 
     if i % 10 == 0:
-        model.save_weights("checkpoints/%d" % i)
-        plt.savefig("checkpoints/%d.png" % i)
+        model.save_weights("checkpoints/fusion%d" % i)
+        plt.savefig("checkpoints/fusion%d.png" % i)
     i += 1
 
     if (i * data_batch_size) >= epochs * n_samples:
